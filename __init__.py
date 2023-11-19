7adf3f790052c1cd84500b94dad73b189963a4f4
Max Klaversma <maxklaversma@gmail.com>
2023-11-19 21:21:46 +0100

    Tested app & make small changes.

--- __init__.py
+++ __init__.py
@@ -2,6 +2,7 @@ from pyplanet.apps.config import AppConfig
 from pyplanet.contrib.command import Command
 
 from pyplanet.apps.core.maniaplanet import callbacks as mp_signals
+from pyplanet.apps.core.trackmania import callbacks as tm_signals
 
 
 class SCLDivisionSupport(AppConfig):
@@ -12,6 +13,8 @@ class SCLDivisionSupport(AppConfig):
 	def __init__(self, *args, **kwargs):
 		super().__init__(*args, **kwargs)
 
+		self.is_warming_up = False
+
 		self.setting_type = 'solo'
 		self.setting_team_count = 4
 		self.setting_player_count = 1
@@ -19,6 +22,9 @@ class SCLDivisionSupport(AppConfig):
 	async def on_start(self):
 		self.context.signals.listen(mp_signals.map.map_start, self.map_start)
 		self.context.signals.listen(mp_signals.flow.podium_start, self.podium_start)
+		self.context.signals.listen(tm_signals.warmup_start, self.warmup_start)
+		self.context.signals.listen(tm_signals.warmup_start_round, self.warmup_start)
+		self.context.signals.listen(tm_signals.warmup_end, self.warmup_end)
 
 		await self.instance.command_manager.register(
 			Command(command='type', namespace='scl', target=self.chat_type, admin=False,
@@ -38,8 +44,14 @@ class SCLDivisionSupport(AppConfig):
 		await self.set_server_settings()
 
 	async def podium_start(self, *args, **kwargs):
-		message = '$06fDo not forget to make a screenshot and submit the results on the competition website!'
+		message = '$o$ff0Do not forget to make a screenshot and submit the results on the competition website!'
 		await self.instance.chat(message)
+
+	async def warmup_start(self, **kwargs):
+		self.is_warming_up = True
+
+	async def warmup_end(self, **kwargs):
+		self.is_warming_up = False
 
 	async def chat_type(self, player, data, **kwargs):
 		# Change the current competition type (solo/team).
@@ -72,6 +84,11 @@ class SCLDivisionSupport(AppConfig):
 		await self.set_server_settings()
 
 	async def chat_end_round(self, player, data, **kwargs):
+		if self.is_warming_up:
+			message = '$i$f00You cannot force the end of the warm-up round!'
+			await self.instance.chat(message, player)
+			return
+
 		# Update the mode settings to up the amount of rounds per map.
 		mode_settings = await self.instance.mode_manager.get_settings()
 		mode_settings['S_RoundsPerMap'] = int(mode_settings['S_RoundsPerMap']) + 1
@@ -80,7 +97,10 @@ class SCLDivisionSupport(AppConfig):
 		# End the round + inform players.
 		await self.instance.gbx.multicall(
 			self.instance.gbx('Trackmania.ForceEndRound', encode_json=False, response_id=False),
-			self.instance.chat('$fff{}$z$s$ff0 has forced the current round to end and increased the amount of rounds to play.'.format(player.nickname))
+			self.instance.chat('$fff{}$z$s$ff0 ended the round and increased the amount of rounds to play to $fff{}$ff0.'.format(
+				player.nickname,
+				mode_settings['S_RoundsPerMap']
+			))
 		)
 
 	async def set_server_settings(self):
@@ -104,10 +124,10 @@ class SCLDivisionSupport(AppConfig):
 		mode_settings['S_PointsRepartition'] = points_repartition
 		await self.instance.mode_manager.update_settings(mode_settings)
 
-		message = '$06fCurrent matchsettings: $fff{}$06f [{}players: {}$06f]'.format(
+		message = '$06fCurrent matchsettings: $fff{}$06f [{}players: $fff{}$06f]'.format(
 			self.setting_type,
 			'teams: $fff{}$06f, '.format(self.setting_team_count) if self.setting_type == 'team' else '',
-			'players: {}'.format(self.setting_player_count)
+			self.setting_player_count
 		)
 		await self.instance.chat(message)
 
@@ -135,7 +155,7 @@ class SCLDivisionSupport(AppConfig):
 				repartition = '12, 10, 8, 6, 4, 3, 2, 1'
 			elif self.setting_player_count == 3:
 				repartition = '12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1'
-		elif self.setting_team_count == 5:
+		elif self.setting_team_count >= 5:
 			if self.setting_player_count == 1:
 				repartition = '15, 12, 9, 6, 3'
 			elif self.setting_player_count == 2:
@@ -148,9 +168,13 @@ class SCLDivisionSupport(AppConfig):
 	async def determine_solo_points_repartition(self):
 		repartition = ''
 
-		if self.setting_player_count == 4:
+		if self.setting_player_count == 2:
+			repartition = '6, 3'
+		elif self.setting_player_count == 3:
+			repartition = '9, 6, 3'
+		elif self.setting_player_count == 4:
 			repartition = '12, 9, 6, 3'
-		elif self.setting_player_count == 5:
+		elif self.setting_player_count >= 5:
 			repartition = '15, 12, 9, 6, 3'
 
 		return repartition
